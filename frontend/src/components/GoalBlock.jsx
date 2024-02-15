38ff2f93487021a80253b303c27198eecdf19ac2
jaimin1503 <123434935+jaimin1503@users.noreply.github.com>
2024-02-15 12:03:02 +0530

    styling

--- frontend/src/components/GoalBlock.jsx
+++ frontend/src/components/GoalBlock.jsx
@@ -33,38 +33,43 @@ const GoalBlock = () => {
           Set new goal +
         </button>
       </div>
-      <div className="goal w-[80vw] bg-yellow-300">
+      <div className="goal w-[82vw] bg-purple-50 rounded-2xl mx-auto">
         {goals.map((goal, index) => (
-          <div key={goal._id} className=" py-5">
+          <div key={goal._id} className=" py-10">
             <div className=" flex flex-col items-center">
-              <h1 className=" text-3xl py-3">{goal?.title}</h1>
+              <h1 className=" text-3xl py-3 bg-blue-300 px-5 rounded-2xl">
+                {goal?.title}
+              </h1>
               <p className=" py-2 pb-5">{goal?.description}</p>
             </div>
-            <div className="">
+            <div className=" flex justify-around">
               {goal?.subjects.map((subject, sub_idx) => (
                 <div
                   key={subject._id}
-                  className=" bg-purple-200 max-w-xs flex items-start"
+                  className=" max-w-xs bg-purple-100 rounded-2xl flex items-start"
                 >
                   <div className=" flex my-5 flex-col items-start">
-                    <div className=" text-2xl py-3 mx-auto">
-                      {sub_idx}-{subject.title}
+                    <div className=" text-2xl py-3 px-5 rounded-2xl mx-auto bg-purple-300">
+                      {subject.title}
                     </div>
 
                     {subject?.topics.map((topic, tp_idx) => (
-                      <div key={topic._id} className="topics flex py-3 mx-5">
+                      <div
+                        key={topic._id}
+                        className="topics flex py-1 items-center mx-5 my-2"
+                      >
                         <select
                           name="status"
                           id="status"
                           value={topic?.status}
-                          className=" outline-none bg-transparent border border-purple-700 p-1 rounded-2xl text-"
+                          className=" outline-none bg-transparent border border-purple-700 p-1 rounded-2xl"
                         >
                           <option value="Done">Done</option>
                           <option value="Pending">Pending</option>
                           <option value="Review">Review</option>
                         </select>
                         <h1 className="mx-3">
-                          {tp_idx}-{topic.title}
+                          {tp_idx} - {topic.title}
                         </h1>
                       </div>
                     ))}
